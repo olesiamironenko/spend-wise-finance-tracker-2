4d48e1b5f0ab641b49a52360ee1b60b492ae2ddb
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2026-02-27 18:00:17 -0500

    user model added

---
+++ backend/src/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [ true, "Name is required" ],
+      trim: true,
+      minLenght: [ 3, "Name must be at least 3 characters" ],
+      maxLenght: [ 50, "Name must be less than 50 characters" ]
+    }, 
+    email: {
+      type: String,
+      required: [ true, "Email is required" ],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [ /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please provide a valid email address" ]
+    },
+    password: {
+      type: String,
+      required: [ true, "Password is required" ],
+      minLenght: [ 6, "Password must be at least 6 characters" ],
+      maxLenght: [ 100, "Password must be less than 100 characters" ],
+      select: false // don't return password by default
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash the password before saving the user (only if the password is modified
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare entered password with hashed password
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
