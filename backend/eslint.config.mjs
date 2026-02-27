2016c8861ebd7511cd3e08b0a9c572197e364a96
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2026-02-27 11:04:49 -0500

    basic packages installed for Node backend and React frontend 2

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+
+export default defineConfig([
+  js.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs'
+    }
+  },
+  {
+    ignores: ['node_modules', 'dist', 'build', 'coverage']
+  }
+])
